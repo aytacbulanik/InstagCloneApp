95aaaf6fbe7e8200391ff1bba886097306393ca0
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-14 00:08:21 +0300

    first commit

---
+++ InstagCloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagCloneApp
+//
+//  Created by AYTAÇ BULANIK on 13.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
