2cf52ce1de70746d0aebe8c328d85c2c7fb54c64
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-16 16:24:24 +0300

    wrote create user

---
+++ InstagCloneApp/View/SignUpVC.swift
@@ -0,0 +1,46 @@
+//
+//  SignUpVC.swift
+//  InstagCloneApp
+//
+//  Created by aytaç bulanık on 16.12.2022.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpVC: UIViewController {
+
+    @IBOutlet var mailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var passwordAgainField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        
+        guard let mail = mailField.text , let password = passwordField.text , let password1 = passwordAgainField.text else {return}
+        
+        if mail.isEmpty || password.isEmpty || password1.isEmpty {
+            print("İşleri boş")
+            return
+        }
+        if password != password1 {
+            print("şifreler eşit değil")
+            return
+        }
+        Auth.auth().createUser(withEmail: mail, password: password1) {
+            (result,error) in
+            if error == nil {
+                print("bir hata oluştu", error?.localizedDescription)
+            }
+            
+            
+        }
+    }
+    
+
+}
--- InstagCloneApp/View/ViewController.swift
+++ InstagCloneApp/View/ViewController.swift
@@ -6,22 +6,34 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     
     @IBOutlet var nameField : UITextField!
     @IBOutlet var passwordField : UITextField!
+    @IBOutlet var girisButtonOut : UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let closeTapGesture = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(closeTapGesture)
+        girisButtonOut.layer.cornerRadius = 8
     }
     
     @IBAction func signInButtonPressed(_ sender : UIButton) {
-        performSegue(withIdentifier: "toHome", sender: nil)
+        guard let mail = nameField.text else {return}
+        guard let password = passwordField.text else {return}
+        
+        if mail.isEmpty || password.isEmpty {
+            print("Alanlar boş olamaz")
+        } else {
+            
+            performSegue(withIdentifier: "toHome", sender: nil)
+        }
+        
     }
     @objc func closeKeyboard() {
         view.endEditing(true)
