267191d6ec87eee24ec84548a332255b7fdb56c0
Aytaç Bulanık <aytacbulanik@hotmail.com>
2022-12-24 11:53:50 +0300

    data turn with struct in firebase

---
+++ InstagCloneApp/View/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  InstagCloneApp
+//
+//  Created by AYTAÇ BULANIK on 24.12.2022.
+//
+
+import Foundation
+
+struct firebaseColConstant {
+    static let posts = "Posts"
+}
+
+struct firebaseFieldConstant {
+    static let date = "date"
+    static let imageUrl = "imageUrl"
+    static let likes = "likes"
+    static let postComment = "postComment"
+    static let postedBy = "postedBy"
+}
---
+++ InstagCloneApp/View/PostStruct.swift
@@ -0,0 +1,17 @@
+//
+//  PostStruct.swift
+//  InstagCloneApp
+//
+//  Created by AYTAÇ BULANIK on 24.12.2022.
+//
+
+import Foundation
+
+struct PostStruct {
+    var postedBy : String
+    var postComment : String
+    var imageUrl : String
+    var likeCount : Int
+    var postDate : Date
+    var postId : String
+}
